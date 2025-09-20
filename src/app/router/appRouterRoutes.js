fb91535760bd28ec6e70bfe910b9a8dfb98979b4
Romanenko Yliia(mac) <romany0507306677@gmail.com>
2025-09-20 16:46:50 +0300

    modify path

--- src/app/router/appRouterRoutes.js
+++ src/app/router/appRouterRoutes.js
@@ -1,10 +1,19 @@
 import { frontRoutes } from "@/shared/api/config/routes/frontRoutes.js";
 import { Component } from "react";
 
-export const pagesList = Object.keys(frontRoutes.pages);
-export const appRouterRoutes = pagesList.map((page) => ({
+const pages = import.meta.glob("../../pages/*.jsx");
+export const appRouterRoutes = Object.keys(frontRoutes.pages).map((page) => ({
   ...frontRoutes.pages[page],
-  lazy: async () => ({
-    Component: (await import(`../../pages/${page}.jsx`)).default,
-  }),
+  lazy: async () => {
+    const module = await pages[`../../pages/${page}.jsx`]();
+    return { Component: module.default };
+  },
 }));
+// export const pagesList = Object.keys(frontRoutes.pages);
+
+// export const appRouterRoutes = pagesList.map((page) => ({
+//   ...frontRoutes.pages[page],
+//   lazy: async () => ({
+//     Component: (await import(`../../pages/${page}.jsx`)).default,
+//   }),
+// }));
